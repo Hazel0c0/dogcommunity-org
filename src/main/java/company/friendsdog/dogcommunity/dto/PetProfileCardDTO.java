e3e27c9d66b20161091d5e0b81112be0aaf82ca4
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-05-09 16:39:38 +0900

    select/v1.2.0/dto 생성중

--- src/main/java/company/friendsdog/dogcommunity/dto/PetProfileCardDTO.java
+++ src/main/java/company/friendsdog/dogcommunity/dto/PetProfileCardDTO.java
@@ -9,5 +9,11 @@ import lombok.ToString;
 @EqualsAndHashCode
 // 클라이언트 프로필 카드 만들 때 정보
 public class PetProfileCardDTO {
+    private int likes;
+    private int hits;
 
+  public PetProfileCardDTO(int likes, int hits) {
+    this.likes = likes;
+    this.hits = hits;
+  }
 }
