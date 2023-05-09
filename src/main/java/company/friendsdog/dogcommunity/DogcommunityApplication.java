15269421cf60667224811cb3fe1cdb3a12b5b201
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-05-09 15:18:05 +0900

    start

---
+++ src/main/java/company/friendsdog/dogcommunity/DogcommunityApplication.java
@@ -0,0 +1,13 @@
+package company.friendsdog.dogcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogcommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/company/friendsdog/dogcommunity/DogcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package company.friendsdog.dogcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogcommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
