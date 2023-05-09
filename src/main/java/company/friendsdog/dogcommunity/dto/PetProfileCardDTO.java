0933a67e05220ef4ca83a496ee5accfb9f983139
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-05-09 16:21:54 +0900

    select/v1

---
+++ src/main/java/company/friendsdog/dogcommunity/dto/PetProfileCardDTO.java
@@ -0,0 +1,13 @@
+package company.friendsdog.dogcommunity.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+// 클라이언트 프로필 카드 만들 때 정보
+public class PetProfileCardDTO {
+
+}
